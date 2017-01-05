09ccf920cf2d072c72af0c6824d405b6fecd28a4
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-05 11:34:16 -0500

    set up routes and views for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get '/about', to: 'about#index'
+  get '/area_of_practice', to: 'area_of_practice#index'
+  get '/testimonials', to: 'testimonials#index'
+  get '/location', to: 'location#index'
+
 end
