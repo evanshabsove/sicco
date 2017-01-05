42da30afc0dbd4bdc6851f4b0d07c3bdbc84a89e
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-05 11:21:01 -0500

    initialized controllers for all the links

---
+++ app/controllers/area_of_practice_controller.rb
@@ -0,0 +1,2 @@
+class AreaOfPracticeController < ApplicationController
+end
---
+++ test/controllers/area_of_practice_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AreaOfPracticeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
