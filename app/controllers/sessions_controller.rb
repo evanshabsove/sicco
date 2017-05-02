eaf6fb53fc86fab8b0fbc0d1d2d8c5dcbde4e508
Tedmaloney <Tedmaloney@me.com>
2017-05-02 16:20:08 -0300

    added sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by('lower(username) = ?', session_params[:username].downcase)
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_url
+    else
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:username, :password)
+  end
 end
