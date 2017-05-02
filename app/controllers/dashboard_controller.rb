4556270d9833350b90d9e76689a00b75edbe4e78
Tedmaloney <Tedmaloney@me.com>
2017-05-02 16:42:11 -0300

    Only admins are allowed in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,15 @@
 class DashboardController < ApplicationController
 
+  before_filter :check_admin
+
   def index
   end
-  
+
+  private
+  def check_admin
+    unless current_user.admin
+      redirect_to root_url
+    end
+  end
+
 end
